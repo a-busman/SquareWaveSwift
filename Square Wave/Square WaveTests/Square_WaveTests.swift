68220ac17afc9a88d6b2a634623756d654cf49c9
Alex Busman <abusman@axon.com>
2020-02-10 12:47:45 -0800

    Initial Commit

---
+++ Square Wave/Square WaveTests/Square_WaveTests.swift
@@ -0,0 +1,34 @@
+//
+//  Square_WaveTests.swift
+//  Square WaveTests
+//
+//  Created by Alex Busman on 2/10/20.
+//  Copyright © 2020 Alex Busman. All rights reserved.
+//
+
+import XCTest
+@testable import Square_Wave
+
+class Square_WaveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
