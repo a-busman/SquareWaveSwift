26412deec4765a1c16423d85a13f9f55c0ea1a55
Alex Busman <alex.busman@gmail.com>
2020-03-15 13:22:14 -0700

    Add missing header.

---
+++ Square Wave/Square Wave/Engine/EmulatorBackend/Gme.h
@@ -0,0 +1,18 @@
+//
+//  Gme.h
+//  Square Waves
+//
+//  Created by Alex Busman on 3/14/20.
+//  Copyright © 2020 Alex Busman. All rights reserved.
+//
+
+#ifndef Gme_h
+#define Gme_h
+
+#import "EmulatorBackend.h"
+
+@interface Gme : EmulatorBackend
+
+@end
+
+#endif /* Gme_h */
